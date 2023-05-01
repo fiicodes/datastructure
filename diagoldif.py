0f8a57abeef5c3e356f3851d54eb03533d7aa761
fithaashraf17 <fithaashraf172@gmail.com>
2023-05-01 11:27:16 +0530

    updated

---
+++ diagoldif.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    left_diag=0
+    right_diag=0
+    for i in range(len(arr)):
+        left_diag+=arr[i][i]
+        right_diag+=arr[i][len(arr)-1-i]
+    return( abs(left_diag-right_diag))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = (diagonalDifference(arr))
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ palindromeindex.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'palindromeIndex' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def palindromeIndex(s):
+    # Write your code here
+    i=0
+    j=len(s)-1
+    while i<j:
+        if s[i]!=s[j]:
+            if s[i+1:j+1]==s[i+1:j+1][::-1]:
+                return i
+            else:
+               return j
+        i+=1
+        j-=1
+    return -1
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = (palindromeIndex(s))
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
