a40e8c93e6532dab4aa3b18afb02634012f4aeef
fithaashraf17 <fithaashraf172@gmail.com>
2023-11-15 19:32:07 +0530

    updated

--- secondlargestandsmallest.py
+++ secondlargestandsmallest.py
@@ -1,12 +1,28 @@
 def getSecondOrderElements(n: int, a: [int]) -> [int]:
-    sorted_min = sorted(a)
-    res = []
-    res.append(sorted_min[n - 2])
-    res.append(sorted_min[1])
-    return res
+    def findsecondlarge(a, n):
+        largest = a[0]
+        secondlarge = -1
+        for i in range(1, n):
+            if a[i] > largest:
+                secondlarge = largest
+                largest = a[i]
+            elif a[i] < largest and a[i] > secondlarge:
+                secondlarge = a[i]
+        return secondlarge
 
+    def findsecondsmallest(a, n):
+        smallest = a[0]
+        secondsmallest = float("inf")
+        for i in range(1, n):
+            if a[i] < smallest:
+                secondsmallest = smallest
+                smallest = a[i]
+            elif a[i] > smallest and a[i] < secondsmallest:
+                secondsmallest = a[i]
+        return secondsmallest
 
-print(getSecondOrderElements(5, [1, 2, 3, 4, 5]))
+    secondlargest = findsecondlarge(a, n)
+    secondsmallest = findsecondsmallest(a, n)
+    return [secondlargest, secondsmallest]
 
-
-# Write your code here.
+    # Write your code here.
