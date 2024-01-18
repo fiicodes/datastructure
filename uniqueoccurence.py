34c8426b546542da73e9df24cf9d620177d530ec
fithaashraf17 <fithaashraf172@gmail.com>
2024-01-18 10:42:38 +0530

    updated

---
+++ uniqueoccurence.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def uniqueOccurrences(self, arr):
+        hashMap = {}
+
+        # Count occurrences of each element in arr
+        for num in arr:
+            if num in hashMap:
+                hashMap[num] += 1
+            else:
+                hashMap[num] = 1
+
+        hashSet = set()
+        flag = True
+
+        # Check if there are unique occurrences
+        for count in hashMap.values():
+            if count in hashSet:
+                flag = False
+                break
+            else:
+                hashSet.add(count)
+
+        return flag
+
+
+
+        
