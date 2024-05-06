c91e4a5ea2743a659f75195208c02da2abe9100f
fithaashraf17 <fithaashraf172@gmail.com>
2024-05-06 21:51:22 +0530

    6-5-2024

---
+++ deletenode.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+
+        # Copy the value of the next node to the current node
+        node.val = node.next.val
+
+        # Skip the next node by updating the current node's next pointer
+        node.next = node.next.next
