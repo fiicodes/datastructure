6ce808b3fccfcb3ec084cffca8f1d90943c3f5d4
fithaashraf17 <fithaashraf172@gmail.com>
2024-09-08 11:50:41 +0530

    08-09-2024

---
+++ firstandlastoccurance.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def findTarget(nums, target, first):
+            l = 0
+            h = len(nums) - 1
+            result = -1
+            while (l <= h):
+                mid = (l + h) // 2
+
+                if nums[mid] == target:
+                    result = mid
+                    if first:
+
+                        h = mid - 1
+
+                    else:
+
+                        l = mid + 1
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    h = mid - 1
+            return result
+
+        firstoccur = findTarget(nums, target, True)
+        if firstoccur == -1:
+            return [-1, -1]
+        secondoccur = findTarget(nums, target, False)
+        return [firstoccur, secondoccur]
+
+
+
+
