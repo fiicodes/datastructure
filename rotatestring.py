de8a6570a3bb7becf4368ae428cce43db6772b60
fiicodes <fithaashraf172@gmail.com>
2024-11-05 11:51:37 +0530

    5-11-2024

---
+++ rotatestring.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        double_s = s + s
+        return goal in double_s
+
+
+
+
+
+
